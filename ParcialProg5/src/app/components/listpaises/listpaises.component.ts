7e54e581814811cdc39e5e1a956bfa66b6b6cd4c
zaratorner <juanmatiasmoralez@gmail.com>
2025-06-23 19:11:41 -0300

    Co-authored-by: Owen Vonkunosky <Owen-Vonkunosky@users.noreply.github.com>

--- ParcialProg5/src/app/components/listpaises/listpaises.component.ts
+++ ParcialProg5/src/app/components/listpaises/listpaises.component.ts
@@ -1,18 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { PaisesService } from '../../services/paises.service';
-
+import { CommonModule, NgIf, NgFor } from '@angular/common';
 @Component({
   selector: 'app-listpaises',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule,NgIf,NgFor],
   templateUrl: './listpaises.html',
   styleUrl: './listpaises.css'
 })
 export class ListpaisesComponent implements OnInit{
-  constructor(public PaisesService: PaisesService){}
-  
-  ngOnInit() {
-    this.PaisesService.getAllpaises()
-  }
+countries: any[] = [];
+errorMessage: string = '';
+loading: boolean = true;
+  constructor(private paisesService: PaisesService){}
+  ngOnInit(): void {
+    this.paisesService.getAllpaises().subscribe({
+      next: (data) => {
+        this.countries = data;
+        this.loading = false; // Datos cargados, detiene el estado de carga
+      },
+      error: (error) => {
+        console.error('Error al obtener países:', error);
+        this.errorMessage = 'No se pudieron cargar los países. Por favor, inténtalo de nuevo más tarde.';
+        this.loading = false; // La carga finalizó incluso con un error
+      }
+    });  
+ 
  
 
-}
+}}
--- ParcialProg5/src/app/services/paises.service.ts
+++ ParcialProg5/src/app/services/paises.service.ts
@@ -1,17 +1,26 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PaisesService {
+  private API_BASE_URL = 'https://restcountries.com/v3.1';
 
   constructor(private Http: HttpClient) { 
-   this.Http.get('https://restcountries.com')
   }
-  getAllpaises(){
-    this.Http.get('https://restcountries.com')
-    
+  getAllpaises(): Observable<any[]>{
+    const fields = 'name,capital,region,population,flags,ccn3';
+    return this.Http.get<any[]>(`${this.API_BASE_URL}/all?fields=${fields}`)
    }
+
+   getPaisesByName(name : String): Observable<any[]>{
+    return this.Http.get<any[]>(`https://restcountries.com/v3.1/name/${name}`)
+   }
+
+   getPaisesByRegion(region: string): Observable<any[]> {
+    return this.Http.get<any[]>(`https://restcountries.com/v3.1/region/${region}`);
+  }
 
 }
